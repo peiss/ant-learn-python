cf5885f01341da565e9386f7aea342c5e83fcb9f
peiss <cztps2@gmail.com>
2020-04-02 00:36:56 +0800

    'xx'

---
+++ 2020-04-01_python_image_cartoon_luoyonghao/python制作卡通图片.py
@@ -0,0 +1,9 @@
+import cv2
+
+img = cv2.imread("luoyonghao.jpg")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 3)
+edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 7)
+
+cv2.imwrite("./output_luoyonghao.jpg", edges)
